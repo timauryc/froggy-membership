828fc42c934e1de728776e8cc9b7778f61f435a0
carlos.timaury <carlos.timaury@minutrade.com>
2019-11-06 19:04:34 -0300

    first commit

---
+++ test/sample.js
@@ -0,0 +1,9 @@
+var assert = require("assert")
+
+describe("something", function () {
+    describe("the way its smells", function () {
+        it("is pungent", function () {
+            assert.ok(true)
+        })
+    })
+})
